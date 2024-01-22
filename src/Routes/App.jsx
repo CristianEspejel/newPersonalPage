d9a30fe83f303a59e0bd5722022173ef35195796
CristianEspejel <tapiaxto@gmail.com>
2024-01-22 15:09:36 -0600

    verificacion de favicon

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,25 +1,23 @@
+ import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../pages/Home';
+import Header from '../Components/Header/Header';
 
-                                import { BrowserRouter, Routes, Route } from "react-router-dom";
-                                import Home from '../pages/Home';
-                                import Header from '../Components/Header/Header';
-                                import Footer from '../Components/Footer/Footer'
-                                import '../Styles/App.css'
+import '../Styles/App.css'
 
-                                function App() {
+function App() {
                                   
-                                  return (
-                                    <div className="App">
-                                    <BrowserRouter>
-                                    <Header/>
-                                    <Routes>
-                                        <Route path = "/" element = {<Home/>}/>
-                                    </Routes>
-                                    </BrowserRouter>
+      return (
+              <div className="App">
+                  <BrowserRouter>
+                    <Header/>
+                      <Routes>
+                        <Route path = "/" element = {<Home/>}/>
+                      </Routes>
+                  </BrowserRouter>
 
-                                    <Footer/>
                                     
-                                  </div>
-                                  )
-                                }
+             </div>
+          )
+        }
 
-                                export default App
+export default App
